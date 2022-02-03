f79f865b3e185e9ddd083cb6fede15c049924c4d
Sam Davenport <samueldavenport@att.net>
2022-02-03 11:35:44 -0600

    added tests to Modal component. they all pass

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,43 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '../../Modal';
+
+const currentPhoto = {
+    name: 'Park bench',
+    category: 'landscape',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    index: 1
+}
+const mockToggleModal = jest.fn();
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    it('renders', () => {
+        render(<Modal
+            currentPhoto={currentPhoto}
+            onClose={mockToggleModal}
+          />)
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Modal
+            currentPhoto={currentPhoto}
+            onClose={mockToggleModal}
+          />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
+
+describe('Click Event', () => {
+    it('calls onClose handler', () => {
+        const {getByText} = render(<Modal
+            currentPhoto={currentPhoto}
+            onClose={mockToggleModal}
+          />)
+          fireEvent.click(getByText('Close this modal'));
+        expect(mockToggleModal).toHaveBeenCalledTimes(1);
+    });
+  })  
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Modal({currentPhoto}) {
+function Modal({currentPhoto, onClose}) {
     const { name, category, description, index} = currentPhoto;
 
     return (
@@ -9,7 +9,7 @@ function Modal({currentPhoto}) {
           <h3 className="modalTitle">{name}</h3>
           <img alt={category} src={require(`../../assets/small/${category}/${index}.jpg`)} />
           <p>{description}</p>
-          <button type="button">Close this modal</button>
+          <button type="button" onClick={onClose}>Close this modal</button>
         </div>
       </div>
     );
